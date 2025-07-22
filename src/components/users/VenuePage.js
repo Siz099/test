78171647ea7dcb1a0b6acb274ee92cf470102b61
himhello <himhello669@gmail.com>
2025-07-22 07:56:45 +0545

    Updated VenuePage.js

--- src/components/users/VenuePage.js
+++ src/components/users/VenuePage.js
@@ -1,18 +1,1 @@
-@@ .. @@
-import React, { useState, useEffect } from "react";
-+import { Link } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
-import PopularVenues from "./PopularVenues";
-import HeroSection from "./HeroSection";
-import { venueService, imageService } from "../../services/api";
-import "../../styles/VenuePage.css";
-@@ .. @@
-                  <div className="stars">★★★★★</div>
--                  <button className="view-details-button">View Details</button>
-+                  <Link to={`/venues/${venue.venue_id}`} className="view-details-button">
-+                    View Details
-+                  </Link>
-                </div>
-              </div>
-            ))}
+{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
